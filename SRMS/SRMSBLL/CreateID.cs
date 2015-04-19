28eccd18f67cbfa509d7c5791b9a1b3dbba3d6d3
huangpu <419468419@qq.com>
2015-04-19 10:12:20 +0800

    2015/4/19

---
+++ SRMS/SRMSBLL/CreateID.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SRMSBLL
+{
+    class CreateID
+    {
+        private string newsID;
+        private CurrentTime ct = new CurrentTime();
+        public string getNewsID()
+        {
+
+            return newsID;
+        }
+        private string mend2(string number)
+        {
+            int temp = int.Parse(number)/10;
+            if (temp == 0)
+            {
+                number = "0" + number;
+            }
+            return number;
+        }
+        private string mend3(string number)
+        {
+            int temp = int.Parse(number) / 10;
+            if (temp == 0)
+            {
+                number = "00" + number;
+            }
+            else if (temp > 0 && temp <= 9)
+            {
+                number = "0" + number;
+            }
+            return number;
+        }
+    }
+}
---
+++ SRMS/SRMSBLL/CurrentTime.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace SRMSBLL
+{
+    class CurrentTime
+    {
+        private DateTime dt = new DateTime();
+        private string year;
+        private string month;
+        private string date;
+        private string hour;
+        private string minute;
+        private string secord;
+
+
+
+
+        public string Year
+        {
+            get 
+            {
+                year = dt.Year.ToString();
+                return year; 
+            }
+        }
+
+        public string Month
+        {
+            get 
+            {
+                month = dt.Month.ToString();
+                return month; 
+            }
+        }
+
+        public string Date
+        {
+            get 
+            {
+                date = dt.Date.ToString();
+                return date; 
+            }
+        }
+
+        public string Hour
+        {
+            get 
+            {
+                hour = dt.Hour.ToString();
+                return hour; 
+            }
+        }
+
+        public string Minute
+        {
+            get 
+            {
+                minute = dt.Minute.ToString();
+                return minute; 
+            }
+        }
+
+        public string Secord
+        {
+            get 
+            {
+                secord = dt.Second.ToString();
+                return secord; 
+            }
+        }
+    }
+}
